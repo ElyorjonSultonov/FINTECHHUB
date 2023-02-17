f0308102ca960429db9ec3d88b1e730d0acdafaa
Elyor <elyorsultonov0707@gmail.com>
2023-02-17 18:22:53 +0500

    navbar end

---
+++ src/Pages/HomePage/Easier/Easier.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+function Easier() {
+  return (
+    <div className="easier">
+      <div className="container">
+        <div className="row">
+          <div className="easierText">
+            <h1>Endi o‘rganish osonroq</h1>
+          </div>
+          <div className="line"></div>
+          <div className="easierBtn">
+            <button>
+              O‘RGANISHNI <br /> BOSHLASH
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Easier;
