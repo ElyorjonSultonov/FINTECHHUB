34c2d2a7587fbbbd843da352cfde1defcdc06ac1
Elyor <elyorsultonov0707@gmail.com>
2023-02-18 09:51:31 +0500

    About us to add

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import AboutUs from "./AboutUs/AboutUs";
 import Easier from "./Easier/Easier";
 
 function HomePage() {
   return <div>
     <Easier/>
+    <AboutUs/>
   </div>;
 }
 
